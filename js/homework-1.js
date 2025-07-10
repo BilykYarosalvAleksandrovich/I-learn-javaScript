6ee37a8a6c49c3f9456d8262914b36e6a5e8c6a6
BilykYarosalvAleksandrovich <bilyk.yaroslav.aleksandrovich@gmail.com>
2025-07-10 17:55:09 +0300

    my second code about age

--- js/homework-1.js
+++ js/homework-1.js
@@ -1,7 +1,13 @@
-const myName = 'Yaroslav';
-const currentYear = 2025;
-const isStudent = true;
+// const myName = 'Yaroslav';
+// const currentYear = 2025;
+// const isStudent = true;
 
-console.log(myName);
-console.log(currentYear);
-console.log(isStudent);
+// console.log(myName);
+// console.log(currentYear);
+// console.log(isStudent);
+
+const birthYear = 1991;
+const currentYear = 2025;
+const age = currentYear - birthYear;
+console.log(age);
+console.log(`Мені ${age} роки`);
