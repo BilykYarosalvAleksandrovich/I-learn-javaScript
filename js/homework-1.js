ef5fa1150ee2a369380396bf14a9194d83618b6e
BilykYarosalvAleksandrovich <bilyk.yaroslav.aleksandrovich@gmail.com>
2025-07-11 09:58:04 +0300

    my new homework-3

--- js/homework-1.js
+++ js/homework-1.js
@@ -6,8 +6,23 @@
 // console.log(currentYear);
 // console.log(isStudent);
 
-const birthYear = 1991;
-const currentYear = 2025;
-const age = currentYear - birthYear;
-console.log(age);
-console.log(`Мені ${age} роки`);
+// const birthYear = 1991;
+// const currentYear = 2025;
+// const age = currentYear - birthYear;
+// console.log(age);
+// console.log(`Мені ${age} роки`);
+
+const age = 34;
+
+if (age >= 18) {
+  console.log('Ти дорослий');
+} else {
+  console.log('Ти ще неповнолітній');
+}
+
+const name = 'Ярослав';
+if (name === 'Ярослав' && age >= 30) {
+  console.log('Привіт, досвідчений Ярослав!');
+} else {
+  console.log('Привіт, гість!');
+}
