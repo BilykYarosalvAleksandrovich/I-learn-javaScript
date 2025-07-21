104a03a7f92a119480e4e87094547f3542cdafbc
BilykYarosalvAleksandrovich <bilyk.yaroslav.aleksandrovich@gmail.com>
2025-07-21 17:28:14 +0300

    new message

--- js/homework-1.js
+++ js/homework-1.js
@@ -211,3 +211,9 @@ function printLibrary(library) {
   }
 }
 printLibrary(library);
+
+const planets = ['Earth', 'Mars', 'Venus'];
+
+for (let i = 0; i < planets.length; i += 1) {
+  console.log(planets[i]);
+}
