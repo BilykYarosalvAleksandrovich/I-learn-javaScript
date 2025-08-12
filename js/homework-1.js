01294efbabaa58d1e8107e1f503d9e331333bf02
BilykYarosalvAleksandrovich <bilyk.yaroslav.aleksandrovich@gmail.com>
2025-08-12 10:54:40 +0300

    module5

--- js/homework-1.js
+++ js/homework-1.js
@@ -1056,4 +1056,138 @@
 
 // ланцюжки методів
 
-const arr = [1, 5, 89, 3, 66];
+// const arr = [1, 5, 89, 3, 66];
+
+// const res = arr.filter(item => item > 2);
+
+// const res1 = res.map(number => number * 3);
+
+// const res2 = res1.toSorted((a, b) => a - b);
+
+// console.log(res2);
+// const result = arr
+//   .filter(item => item > 2)
+//   .map(item => item * 3)
+//   .toSorted((a, b) => a - b);
+
+// console.log(result);
+
+// const allCars = [
+//   {
+//     make: 'Honda',
+//     model: 'CR-V',
+//     type: 'suv',
+//     amount: 14,
+//     price: 24045,
+//     onSale: true,
+//   },
+//   {
+//     make: 'Honda',
+//     model: 'Accord',
+//     type: 'sedan',
+//     amount: 2,
+//     price: 22455,
+//     onSale: true,
+//   },
+//   {
+//     make: 'Mazda',
+//     model: 'Mazda 6',
+//     type: 'sedan',
+//     amount: 8,
+//     price: 24195,
+//     onSale: false,
+//   },
+//   {
+//     make: 'Mazda',
+//     model: 'CX-9',
+//     type: 'suv',
+//     amount: 7,
+//     price: 31520,
+//     onSale: true,
+//   },
+//   {
+//     make: 'Toyota',
+//     model: '4Runner',
+//     type: 'suv',
+//     amount: 19,
+//     price: 34210,
+//     onSale: false,
+//   },
+//   {
+//     make: 'Toyota',
+//     model: 'Sequoia',
+//     type: 'suv',
+//     amount: 16,
+//     price: 45560,
+//     onSale: false,
+//   },
+//   {
+//     make: 'Toyota',
+//     model: 'Tacoma',
+//     type: 'truck',
+//     amount: 4,
+//     price: 24320,
+//     onSale: true,
+//   },
+//   {
+//     make: 'Ford',
+//     model: 'F-150',
+//     type: 'truck',
+//     amount: 11,
+//     price: 27110,
+//     onSale: true,
+//   },
+//   {
+//     make: 'Ford',
+//     model: 'Fusion',
+//     type: 'sedan',
+//     amount: 13,
+//     price: 22120,
+//     onSale: true,
+//   },
+//   {
+//     make: 'Ford',
+//     model: 'Explorer',
+//     type: 'suv',
+//     amount: 6,
+//     price: 31660,
+//     onSale: false,
+//   },
+// ];
+// нехай функція getModelsOnSail повертає масив моделей автомобілів, але тільки тих, які зараз на розпродажі
+
+// const getModelsOnSale = cars => {
+//   return cars.filter(car => car.onSale).map(car => car.model);
+// };
+
+// console.log(getModelsOnSale(allCars));
+
+// нехай функція getSortedCarsOnSale повертає масив автомобілів, на розпродажі (властивість onSale), відсортованих за зростанням ціни
+
+// const getSortedCarsOnSale = cars => {
+//   return cars
+//     .filter(item => item.onSale)
+//     .toSorted((a, b) => a.price - b.price)
+//     .map(item => {
+//       return {
+//         model: item.model,
+//         price: item.price,
+//       };
+//     });
+// };
+
+// console.log(getSortedCarsOnSale(allCars));
+
+// const arr = ['apple,😁', 'orange,😎', 'banana,😊', 'lemon,😉'];
+
+// const obj = arr.reduce((acc, item) => {
+//   const arr = item.split(',');
+
+//   const key = arr[0];
+//   const value = arr[1];
+
+//   acc[key] = value;
+//   return acc;
+// }, {});
+
+// console.log(obj);
